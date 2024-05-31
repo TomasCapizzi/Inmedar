fc1ee6e3140c3bbcd6bbe5546b3c295ededb168c
Tomas Capizzi <tomas.capizzi@hotmail.com>
2024-05-31 13:54:04 -0300

    Create productos.ts

---
+++ src/data/productos.ts
@@ -0,0 +1,17 @@
+export const productos = [
+    {
+        id:1,
+        img: './img/productos/tibial2.png',
+        name: 'Tibial 2'
+    },
+    {
+        id:1,
+        img: './img/productos/tibial4.png',
+        name: 'Tibial 4'
+    },
+    {
+        id:1,
+        img: './img/productos/tibial6.png',
+        name: 'Tibial 6'
+    },
+];
