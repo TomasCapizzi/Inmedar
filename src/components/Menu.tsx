6381fd70e510d126560d57735c4f76ef8a277bfe
Tomas Capizzi <tomas.capizzi@hotmail.com>
2024-06-11 14:07:13 -0300

    Add links to list

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
-
 //type Props = {}
+
+import { Link } from "react-router-dom"
 
 export default function Menu() {
   return (
     <nav className='navbar'>
+      <Link to={'/'}>
         <img src="./img/logo/INMEDAR PNG.png" alt="inmedar logotipo" />
-        <ul>
+      </Link>
+      <ul>
+          <a href="#inmedar">
             <li>¿Quiénes somos?</li>
+          </a>
+          <Link to={'/impresora'}>
+            <li>Implantes</li>
+          </Link>
+          <Link to={'/productos'}>
             <li>Productos</li>
-            <li>Contacto</li>
-        </ul>
+          </Link>
+          <a href="#contacto"><li>Contacto</li></a>
+      </ul>
     </nav>
   )
 }
