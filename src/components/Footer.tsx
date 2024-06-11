7319feb678eb7315c65b26178388f357563f65e1
Tomas Capizzi <tomas.capizzi@hotmail.com>
2024-06-11 14:03:22 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,8 +7,9 @@ export default function Footer() {
       <div className='footer-container'>
         <img src="./img/logo/INMEDAR PNG.png" alt="logo inmedar" />
         <div className='footer-contacto'>
-          <h5>11 1234-5678</h5>
-          <h5>inmedar@ejemplo.com.ar</h5>
+          <h5>+54-11-6686-0941</h5>
+          <h5>info@inmedar.com</h5>
+          <h5>ventas@inmedar.com</h5>
           <h5>Av. Lisandro de la Torre 2371</h5>
         </div>
       </div>
