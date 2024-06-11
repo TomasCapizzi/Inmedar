44245fa20f02ca5368c6a611b142efc14d102d5b
Tomas Capizzi <tomas.capizzi@hotmail.com>
2024-06-11 14:01:57 -0300

    Add routing and views

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Menu from './components/Menu';
 import './styles/_main.scss';
 import Home from './views/Home';
+import Printer from './views/Printer';
+import Footer from './components/Footer';
+import Productos from './views/Productos';
 
 function App() {
 
   return (
-    <>
-      <Menu/>
-      <Home/>
+    <>      
+      <BrowserRouter>
+        <Menu/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/impresora' element={<Printer/>}/>
+          <Route path='/productos' element={<Productos/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+
+
     </>
   )
 }
